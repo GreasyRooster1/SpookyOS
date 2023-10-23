3ec18585314160489e1a08c01a73b9ceae884877
QuantumDeity <greasyRooster1@gmail.com>
2023-10-22 17:25:54 -0700

    Added the most important command

---
+++ SpookyOS/Commands/ClearCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpookyOS.Commands
+{
+    public class ClearCommand :Command
+    {
+        public ClearCommand() {
+            identifier = "clear";
+            description = "clears the screen";
+            usage = "clear";
+        }
+        public override void Run(Kernel k, string[] args)
+        {
+            Console.Clear();
+        }
+    }
+}
